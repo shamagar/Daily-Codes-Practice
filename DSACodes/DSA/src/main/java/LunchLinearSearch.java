780482bcfa58a7eb33931144b350a76bc2d91629
Sha <Khapangisha@yahoo.com>
2026-04-13 19:41:44 -0500

    DSA code

--- DSACodes/DSA/src/main/java/LunchLinearSearch.java
+++ DSACodes/DSA/src/main/java/LunchLinearSearch.java
@@ -1,3 +1,4 @@
+//This code is demonstration on Linear search on sorted an Array
 
 class Search
 {
@@ -15,17 +16,16 @@ class Search
 public class LunchLinearSearch
 {
     public static void main(String[] args) {
-
+        //Given sorted an array
         int nums [] = {1,2,3,4,5,6,7,8,9,10};
 
         //Initialization the class
         Search search = new Search();
         int result = search.LinearSearch(nums, 9);
-
+        //Validating the result to get the index of target number
         if(result != -1)
             System.out.println("Element found at index " + result);
         else
             System.out.println("Element not found");
-
     }
 }
